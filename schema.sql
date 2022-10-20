806b0c3f0233c23f0de5aa2f0496c86aeadb3aee
niicode <niidarku66@gmail.com>
2022-10-20 11:39:34 +0000

    Add queries to database

--- schema.sql
+++ schema.sql
@@ -10,4 +10,18 @@ CREATE TABLE animals (
     species varchar(100)
 );
 
-ALTER TABLE animals ADD species varchar(100);
+CREATE TABLE owners (
+    id BIGSERIAL PRIMARY KEY,
+    full_name varchar(100),
+    age integer
+);
+
+CREATE TABLE species (
+    id BIGSERIAL PRIMARY KEY,
+    name varchar(100)
+);
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id BIGINT REFERENCES species(id);
+ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners(id);
